594b530c60cd5bc3b69b54d53d220c8eecf84dd7
meli.diaz23 <meli.diaz23@gmail.com>
2024-08-02 06:11:35 -0500

    Refactored functions

--- 02-tic-tac-toe/src/App.jsx
+++ 02-tic-tac-toe/src/App.jsx
@@ -5,7 +5,9 @@ import Log from "./components/Log";
 import { WINNING_COMBINATIONS } from "./winning-combinations";
 import GameOver from "./components/GameOver";
 
-const initialGameBoard = [[null, null, null], [null, null, null], [null, null, null]];
+
+const PLAYERS = { X: 'Player 1', O: 'Player 2' }
+const INITIAL_GAME_BOARD = [[null, null, null], [null, null, null], [null, null, null]];
 
 function deriveActivePlayer(turns) {
   let currentPlayer = 'X';
@@ -15,36 +17,42 @@ function deriveActivePlayer(turns) {
   return currentPlayer;
 }
 
-function App() {
-  const [players, setPlayers] = useState({ X: 'Player 1', O: 'Player 2' });
-  const [gameTurns, setGameTurns] = useState([]);
-  // const [activePlayer, setActivePlayer] = useState('X'); // For avoid use multiple states when is not nedded.
-
-  const currentPlayer = deriveActivePlayer(gameTurns);
-
-  let gameBoard = [...initialGameBoard.map(innerArray => [...innerArray])]; // This way we make a deep copy of the array
-
-  for (const turn of gameTurns) {
-    const { square, player } = turn; // Object destructuring
-    const { row, col } = square;
-    gameBoard[row][col] = player;
-
-  }
-
+function deriveWinner(gameBoard, players) {
   let winner = null;
-
-
   for (const combination of WINNING_COMBINATIONS) {
     const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
     const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
     const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
-
 
     if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol) {
       winner = players[firstSquareSymbol];
     }
   }
 
+  return winner; // It must return some value since is a helper function.
+}
+
+
+function deriveGameBoard(gameTurns) {
+  let gameBoard = [...INITIAL_GAME_BOARD.map(innerArray => [...innerArray])]; // This way we make a deep copy of the array
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn; // Object destructuring
+    const { row, col } = square;
+    gameBoard[row][col] = player;
+  }
+
+  return gameBoard;
+}
+
+function App() {
+  const [players, setPlayers] = useState(PLAYERS);
+  const [gameTurns, setGameTurns] = useState([]);
+  // const [activePlayer, setActivePlayer] = useState('X'); // For avoid use multiple states when is not nedded.
+
+  const currentPlayer = deriveActivePlayer(gameTurns);
+  const gameBoard = deriveGameBoard(gameTurns);
+  const winner = deriveWinner(gameBoard, players);
   const hasDraw = gameTurns.length === 9 && !winner;
 
   function handleSelectSquare(rowIndex, colIndex) {
@@ -67,8 +75,8 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players" className={`highlight-player ${currentPlayer}`}>
-          <Player initialName="Player 1" symbol="X" isActive={currentPlayer === 'X'} onNameChange={handlePlayerNameChange} />
-          <Player initialName="Player 2" symbol="O" isActive={currentPlayer === 'O'} onNameChange={handlePlayerNameChange} />
+          <Player initialName={PLAYERS.X} symbol="X" isActive={currentPlayer === 'X'} onNameChange={handlePlayerNameChange} />
+          <Player initialName={PLAYERS.O} symbol="O" isActive={currentPlayer === 'O'} onNameChange={handlePlayerNameChange} />
         </ol>
         {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart} />}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
